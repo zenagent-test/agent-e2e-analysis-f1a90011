a59028f7325546eb8f0d7f19fec4d5b2b6485b4f
Agent E2E Test <agent-e2e@test.com>
2025-05-29 23:58:26 +0700

    Add button with undefined handler

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={handleClick}>Click me</button>
       </header>
     </div>
   );
